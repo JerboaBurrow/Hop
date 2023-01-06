eec21c1f2e225bebc89e3a5df9cc7be86a5b4073
Jerboa <dev@jerboa.app>
2023-01-06 11:14:30 +0000

    PerlinWorld can be created with any region sizes

--- include/World/world.h
+++ include/World/world.h
@@ -12,15 +12,6 @@
 
 class CollisionDetector;
 
-// cells rendered with marching squares
-const uint64_t RENDER_REGION_SIZE = 128;
-// cells for use in on/offscreen dnamics (physics, rng, etc)
-// TODO mutliple levels of "dynamism"
-const uint64_t DYNAMICS_REGION_SIZE = 128*3;
-// underlying map +1 to account for marching squares
-const uint64_t RENDER_REGION_BUFFER_SIZE = RENDER_REGION_SIZE+1;
-const uint64_t DYNAMICS_REGION_BUFFER_SIZE = DYNAMICS_REGION_SIZE+1;
-
 class World {
 public:
     friend class CollisionDetector;
@@ -30,6 +21,7 @@ public:
     {}
     virtual void draw(Shader & s) = 0;
     virtual void save(std::string filename) = 0;
+    virtual float worldUnitLength() = 0;
 
 protected:
 
@@ -50,16 +42,26 @@ protected:
 
 const float THRESHOLD = 0.2;
 
+// // cells rendered with marching squares
+// const uint64_t RENDER_REGION_SIZE = 128;
+// // cells for use in on/offscreen dnamics (physics, rng, etc)
+// // TODO mutliple levels of "dynamism"
+// const uint64_t DYNAMICS_REGION_SIZE = 128*3;
+// // underlying map +1 to account for marching squares
+// const uint64_t RENDER_REGION_BUFFER_SIZE = RENDER_REGION_SIZE+1;
+// const uint64_t DYNAMICS_REGION_BUFFER_SIZE = DYNAMICS_REGION_SIZE+1;
+
 class PerlinWorld : public World {
 
 public:
-    PerlinWorld(uint64_t s, glm::mat4 p);
+    PerlinWorld(uint64_t s, glm::mat4 p, uint64_t renderRegion, uint64_t dynamicsRegion);
     void draw(Shader & s);
     void save(std::string filename);
     // TexturedQuad getMap(float r = 176., float g = 176., float b = 176.);
     // TexturedQuad getLocalRegionMap();
     void updateRegion(float x, float y);
     void worldToCell(float x, float y, float & ix, float & iy);
+    float worldUnitLength(){return 1.0/RENDER_REGION_SIZE;}
 
     ~PerlinWorld(){
         glDeleteBuffers(1,&VBOquad);
@@ -73,6 +75,8 @@ public:
     }
 
 private:
+
+    const uint64_t RENDER_REGION_SIZE, DYNAMICS_REGION_SIZE, RENDER_REGION_BUFFER_SIZE, DYNAMICS_REGION_BUFFER_SIZE;
 
     glm::mat4 projection;
     
@@ -89,7 +93,6 @@ private:
 
     float minimapSize = 0.25f;
     
-
     Perlin perlin;
 
     GLuint VBOquad, VBOoffset, VBOid, VAO;
