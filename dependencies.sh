5782e4fda9363a15fc7db20e8a22a5b3af68dce5
Jerboa <84378622+Jerboa-app@users.noreply.github.com>
2023-03-01 15:19:28 +0000

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+WINDOWS=1
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -w|--windows)
+      WINDOWS=0
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
 cd include
 if [ -d SFML-2.5.1 ];
 then
@@ -18,6 +36,10 @@ mkdir build && cd build
 cmake .. -D BUILD_SHARED_LIBS=FALSE && make
 export SFML_DIR="../../SFML-2.5.1/build"
 
-cd ..
-mkdir build-windows && cd build-windows
-cmake .. -D BUILD_SHARED_LIBS=FALSE -D CMAKE_TOOLCHAIN_FILE=../../../windows.cmake && make
+
+if [[ $WINDOWS -eq 0 ]];
+then 
+	cd ..
+	mkdir build-windows && cd build-windows
+	cmake .. -D BUILD_SHARED_LIBS=FALSE -D CMAKE_TOOLCHAIN_FILE=../../../windows.cmake && make
+fi
