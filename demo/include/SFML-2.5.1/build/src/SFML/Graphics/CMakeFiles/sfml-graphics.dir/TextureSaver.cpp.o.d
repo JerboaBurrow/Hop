193d214a7234992b1306d094d2bccbddfc266247
Jerboa <84378622+Jerboa-app@users.noreply.github.com>
2023-03-11 19:32:48 +0000

    hop now libHop.a main now a demo (#52)
    * hop now libHop.a
    
    created demo/

---
+++ demo/include/SFML-2.5.1/build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.o.d
@@ -0,0 +1,21 @@
+src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.o: \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Graphics/TextureSaver.cpp \
+ /usr/include/stdc-predef.h \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Graphics/TextureSaver.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Graphics/GLCheck.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Config.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Graphics/GLExtensions.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Graphics/GLLoader.hpp \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h /usr/include/inttypes.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h
---
+++ demo/include/SFML-2.5.1/build/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o.d
@@ -0,0 +1,11 @@
+src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o: \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/GlResource.cpp \
+ /usr/include/stdc-predef.h \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/GlResource.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Config.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/NonCopyable.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/GlContext.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Context.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/ContextSettings.hpp
