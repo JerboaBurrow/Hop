9d1fe4436a13123c79b2a13806400eddd2680043
Jerboa <84378622+Jerboa-app@users.noreply.github.com>
2023-03-11 19:32:48 +0000

    hop now libHop.a main now a demo (#52)
    * hop now libHop.a
    
    created demo/
    
    
    
    Former-commit-id: 193d214a7234992b1306d094d2bccbddfc266247

---
+++ demo/include/SFML-2.5.1/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.o.d
@@ -0,0 +1,14 @@
+src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.o: \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/Sensor.cpp \
+ /usr/include/stdc-predef.h \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Sensor.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Config.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Vector3.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Vector3.inl \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Time.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/SensorManager.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/SensorImpl.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/Unix/SensorImpl.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/NonCopyable.hpp
---
+++ demo/include/SFML-2.5.1/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/SensorImpl.cpp.o.d
@@ -0,0 +1,12 @@
+src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/SensorImpl.cpp.o: \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/Unix/SensorImpl.cpp \
+ /usr/include/stdc-predef.h \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/SensorImpl.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Config.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Sensor.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Window/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Vector3.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Vector3.inl \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Time.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/Window/Unix/SensorImpl.hpp
