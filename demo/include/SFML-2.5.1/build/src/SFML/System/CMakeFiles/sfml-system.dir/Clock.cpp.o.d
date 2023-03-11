434c3306fe2d5e01fec246460c49319473341b63
Jerboa <dev@jerboa.app>
2023-03-11 18:13:45 +0000

    move sfml to demo/sfml
    fix action

---
+++ demo/include/SFML-2.5.1/build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
@@ -0,0 +1,8 @@
+src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/System/Clock.cpp \
+ /usr/include/stdc-predef.h \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Clock.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Export.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/Config.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/include/SFML/System/Time.hpp \
+ /media/harvey/ExtraFiles/Jerboa/Hop/demo/include/SFML-2.5.1/src/SFML/System/Unix/ClockImpl.hpp
