93f36f5443d62923b17a120c3bf7c9adb6d73424
Jerboa <dev@jerboa.app>
2023-03-27 20:18:05 +0100

    add logging to android api

--- include/engine.h
+++ include/engine.h
@@ -217,6 +217,10 @@ namespace Hop
 
         void outputLog(std::ostream & o){o << manager.getLog();}
 
+        #if defined(ANDROID)
+            void outputLogAndroid(){manager.getLog().androidLog();}
+        #endif
+
         // Thread
 
         void requestAddThread(){manager.addThread();}
