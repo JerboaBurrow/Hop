a1a56788b48a8522521330bdd3f0f66929b5ee2f
Jerboa-app <dev@jerboa.app>
2023-08-23 16:11:18 +0100

    delete sfml build

--- demo/desktop/include/SFML-2.5.1/build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o.d
+++
@@ -1,6 +0,0 @@
-src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o: \
- /media/harvey/ExtraFiles/Jerboa/Hop/demo/desktop/include/SFML-2.5.1/src/SFML/System/Time.cpp \
- /usr/include/stdc-predef.h \
- /media/harvey/ExtraFiles/Jerboa/Hop/demo/desktop/include/SFML-2.5.1/include/SFML/System/Time.hpp \
- /media/harvey/ExtraFiles/Jerboa/Hop/demo/desktop/include/SFML-2.5.1/include/SFML/System/Export.hpp \
- /media/harvey/ExtraFiles/Jerboa/Hop/demo/desktop/include/SFML-2.5.1/include/SFML/Config.hpp
