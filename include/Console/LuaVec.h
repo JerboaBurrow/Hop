0171b6206c7d46777315c0885b1cee6107fdb6dd
Jerboa-app <dev@jerboa.app>
2023-12-03 09:49:28 +0000

    adds LuaX data types to abstract getting the data from lua
    each can get from an index or table key string
    
    read methods handle lua stack integrity
    
    
    Former-commit-id: 53fb7cc22776aaf873526b9c9dbab70858323fc7

---
+++ include/Console/LuaVec.h
@@ -0,0 +1,44 @@
+#ifndef LUAVEC
+#define LUAVEC
+
+#include <lua.h>
+#include <vector>
+
+namespace Hop
+{
+
+    struct LuaVec
+    {
+        LuaVec(){}
+
+        void read(lua_State * lua, int index)
+        {
+            elements = getNumericLuaTable(lua, index);
+        }
+
+        bool read(lua_State * lua, const char * name)
+        {
+            int returnType = lua_getfield(lua, 1, name);
+
+            if (returnType == LUA_TTABLE)
+            {
+                read(lua, 2);
+                lua_pop(lua,1);
+                return true;
+            }
+            else
+            {
+                lua_pop(lua,1);
+                return false;
+            }
+        }
+
+        double & operator [] (size_t i){ return elements[i]; }
+
+        size_t size() const { return elements.size(); }
+
+        std::vector<double> elements;
+    };
+}
+
+#endif /* LUAVEC */
