1517a9c031e87d8dc2c36476f8a0d6a4e91ce8c0
Jerboa-app <dev@jerboa.app>
2024-07-28 10:10:43 +0100

    rm comment

--- include/Component/cTransform.h
+++ include/Component/cTransform.h
@@ -6,20 +6,6 @@
 namespace Hop::Object::Component
 {
     using cTransform = jGL::Transform;
-    // struct cTransform {
-    //     double x, y, scale, theta;
-
-    //     cTransform() = default;
-
-    //     cTransform(
-    //         double x,
-    //         double y,
-    //         double t,
-    //         double s
-    //     )
-    //     : x(x), y(y), scale(s), theta(t)
-    //     {}
-    // };
 }
 
 #endif /* CTRANSFORM_H */
