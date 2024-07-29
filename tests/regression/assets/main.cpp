41c2a3bad60c38746cfcc13b411cc8501e241dd4
Jerboa <84378622+Jerboa-app@users.noreply.github.com>
2024-07-29 14:48:00 +0100

    Adds AssetStore, TextureAssetStore (#188)
    * Adds AssetStore, TextureAssetStore
      Adds ctest verbose output

---
+++ tests/regression/assets/main.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+
+    jGL::DesktopDisplay::Config conf;
+    conf.VULKAN = false;
+    conf.COCOA_RETINA = false;
+
+    jGL::DesktopDisplay display(glm::ivec2(1,1),"Test TextureAssetStore", conf);
+    glewInit();
+    std::unique_ptr<jGL::jGLInstance> jGLInstance = std::move(std::make_unique<jGL::GL::OpenGLInstance>(display.getRes()));
+    Hop::Util::Assets::TextureAssetStore textureStore(std::filesystem::path("resource"), jGLInstance);
+    textureStore.scan();
+    for (auto tex : textureStore)
+    {
+        std::cout << tex.first << " ";
+    }
+
+    return 0;
+}
