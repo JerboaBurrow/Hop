f029c2646bdfc3dc40e18d9662c16b6be3ab8915
Jerboa-app <dev@jerboa.app>
2024-11-24 10:31:15 +0000

    Fix add on toRemove

--- include/System/system.h
+++ include/System/system.h
@@ -9,15 +9,31 @@ namespace Hop::System
 {
     using Hop::Object::Id;
 
+    /**
+     * @brief A System operating on objects.
+     *
+     * @remark Each system operates on all objects satisfying a
+     *         given list of components. See Signature.
+     */
     class System
     {
     public:
 
+        /**
+         * @brief Enqueue an object to be added to this System.
+         *
+         * @param id
+         */
         void queueRemove(Id id)
         {
-            toRemove.insert(id);
+            if (objects.find(id) != objects.end()) { toRemove.insert(id); }
         }
 
+        /**
+         * @brief Enqueue an object to be removed from this System.
+         *
+         * @param id
+         */
         void queueAdd(Id id)
         {
             toAdd.insert(id);
--- src/System/systemManager.cpp
+++ src/System/systemManager.cpp
@@ -19,7 +19,7 @@ namespace Hop::System
             }
             else
             {
-                system->queueAdd(i);
+                system->queueRemove(i);
             }
         }
     }
